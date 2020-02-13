124cb42bce2b6a24017ba022c7bf344b6dd9d703
Kumar Himanshu <kumhiman@publicisgroupe.net>
2020-02-13 15:45:19 +0530

    first commit

---
+++ src/homepage.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">FICTIONAL</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">COMICS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">AUTOBIOGRAPHY</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SCI-FI</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">EDUCATIONAL</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>          
+        </div>
+    )
+}
